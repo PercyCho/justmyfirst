eec4318009f8aaa489fbb84ff73f3e1aefae546d
PercyCho <42121899+PercyCho@users.noreply.github.com>
2018-09-03 16:17:50 -0400

    Adding JavaScript and fixing bugs

---
+++ main.js
@@ -0,0 +1,3 @@
+﻿console.log("ok good this works");
+window.alert("By visiting this site, do you agree to share your history and data with the moderators of this site?");
+window.alert("hahaha jk we don't steal ur data");
