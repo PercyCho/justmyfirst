8d022970a74d1b3f8262aa9cff64af264b3d89d1
PercyCho <42121899+PercyCho@users.noreply.github.com>
2018-09-03 16:36:37 -0400

    Fixing the JavaScript
    I'm not a hacker I swear

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
 ﻿console.log("ok good this works");
-window.alert("By visiting this site, do you agree to share your history and data with the moderators of this site?");
-window.alert("hahaha jk we don't steal ur data");
+window.alert("");
